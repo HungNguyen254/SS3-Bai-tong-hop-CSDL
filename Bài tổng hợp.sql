84ebeca3127b44f52eafa20db7037c8894d50299
HungNguyen254 <nguyenhung2540206@gmail.com>
2026-04-16 11:30:15 +0700

    Add files via upload

---
+++ Bài tổng hợp.sql
@@ -0,0 +1,20 @@
+create table products(
+	Product_id varchar(23) primary key,
+    Product_name nvarchar(255),
+    Size varchar(5),
+    price int check(price >= 0)
+);
+insert into products(Product_id,Product_name,Size,price) 
+value ('P01', 'Áo sơ mi trắng', 'L', 250000),
+('P02', 'Quần Jean xanh', 'M', 450000),
+('P03', 'Áo thun Basic', 'XL', 150000),
+('P04', 'Áo hoodie', NULL, -200000);
+Update products
+set price = 400000
+where product_id = 'P02';
+Update Products
+set price = price(1+0,1);
+delete from products
+where product_id = 'P03';
+Select (Product_name,Size) from products
+where price > 300000;
